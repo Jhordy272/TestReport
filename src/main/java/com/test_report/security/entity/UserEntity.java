ffa6f19d574a3da38d30adef702ca7c387e99c1a
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-10-16 21:31:55 -0500

    Add generative strategy

--- src/main/java/com/test_report/security/entity/UserEntity.java
+++ src/main/java/com/test_report/security/entity/UserEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -26,6 +28,7 @@ public class UserEntity implements UserDetails {
 
     @Id
     @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "username")
