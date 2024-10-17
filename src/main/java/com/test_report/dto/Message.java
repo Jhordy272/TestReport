d663b1f62df28fb54645624cc7740fff8eebc79c
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-10-16 21:28:03 -0500

    Create folder dto and dto message

---
+++ src/main/java/com/test_report/dto/Message.java
@@ -0,0 +1,13 @@
+package com.test_report.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Message {
+    String Message;
+}
