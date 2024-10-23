6383c199b967304e81113021d73c1fa625aff9a9
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-10-23 15:43:21 -0500

    Change type find by id ReportRepository

--- src/main/java/com/test_report/repository/ReportRepository.java
+++ src/main/java/com/test_report/repository/ReportRepository.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<ReportEntity, Integer>{
-    public ReportEntity findById(int id);
+    public Optional<ReportEntity> findById(int id);
     public Optional<ReportEntity> findByReportName(String reportName);
 }
