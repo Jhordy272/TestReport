20627ef1317a6464000d11307e31434683dcef2a
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-10-23 17:10:20 -0500

    Create statusDto

---
+++ src/main/java/com/test_report/dto/StatusDto.java
@@ -0,0 +1,13 @@
+package com.test_report.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusDto {
+    private Integer id;
+    private String name;
+}
